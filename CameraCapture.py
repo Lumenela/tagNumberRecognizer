cd04f51ce803b3175ce7453494cbf9fc5b9c534d
StanislavGlebik <glebik.stanislav@gmail.com>
2013-09-23 22:27:13 +0300

    __main__ check added

--- CameraCapture.py
+++ CameraCapture.py
@@ -9,28 +9,26 @@ class VideoCapturer:
     def startCapturing(self):
         pass
 
-cv.NamedWindow("camera", 1)
-capture = cv.CreateCameraCapture(0)
+if __name__ == "__main__":
+    cv.NamedWindow("camera", 1)
+    capture = cv.CreateCameraCapture(0)
 
-width = None #leave None for auto-detection
-height = None #leave None for auto-detection
+    width = None #leave None for auto-detection
+    height = None #leave None for auto-detection
 
-if width is None:
-    width = int(cv.GetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_WIDTH))
-else:
-	cv.SetCaptureProperty(capture,cv.CV_CAP_PROP_FRAME_WIDTH,width)    
+    if width is None:
+        width = int(cv.GetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_WIDTH))
+    else:
+	    cv.SetCaptureProperty(capture,cv.CV_CAP_PROP_FRAME_WIDTH,width)    
 
-if height is None:
-	height = int(cv.GetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_HEIGHT))
-else:
-	cv.SetCaptureProperty(capture,cv.CV_CAP_PROP_FRAME_HEIGHT,height) 
+    if height is None:
+	    height = int(cv.GetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_HEIGHT))
+    else:
+	    cv.SetCaptureProperty(capture,cv.CV_CAP_PROP_FRAME_HEIGHT,height) 
 
-while True:
-    img = cv.QueryFrame(capture)
-    print type(img)
-    print dir(img)
-    cv.ShowImage("camera", img)
-    cv.SaveImage("temp.jpg", img)
-    k = cv.WaitKey(10);
-    if k == 'f':
-        break
+    while True:
+        img = cv.QueryFrame(capture)
+        cv.ShowImage("camera", img)
+        k = cv.WaitKey(10);
+        if k == 'f':
+            break
