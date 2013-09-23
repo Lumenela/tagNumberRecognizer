2946f5b668001a9a4f272967f44ccfed43ddc26c
StanislavGlebik <glebik.stanislav@gmail.com>
2013-09-24 00:55:06 +0300

    Test for series callback added

--- UnitTests.py
+++ UnitTests.py
@@ -2,6 +2,7 @@ import unittest
 import time
 from CameraCapture import VideoCapturer
 from CallBacks import JPEGWriterCallback
+from CallBacks import JPEGSeriesWriterCallback
 
 class VideoCapturerAndCallBacksTestCase(unittest.TestCase):
     def setUp(self):
@@ -14,7 +15,25 @@ class VideoCapturerAndCallBacksTestCase(unittest.TestCase):
         try:
             with open(self.testfilename): pass
         except IOError:
-            assert False, "File doesn't exists!"
+            assert False, "File doesn't exist!"
+
+    def tearDown(self):
+        self.capturer.finishCapturing()
+
+class SeriesCallbackTest(unittest.TestCase):
+    def setUp(self):
+        self.testfileprefix = "testfileprefix"
+        self.capturer = VideoCapturer(JPEGSeriesWriterCallback(self.testfileprefix), interval=1)
+    
+    def test(self):
+        self.capturer.startCapturing()
+        time.sleep(5)
+        try:
+            with open(self.testfileprefix + "000" + ".jpg"): pass
+            with open(self.testfileprefix + "001" + ".jpg"): pass
+            with open(self.testfileprefix + "002" + ".jpg"): pass
+        except IOError:
+            assert False, "File series doesn't exists!"
 
     def tearDown(self):
         self.capturer.finishCapturing()
